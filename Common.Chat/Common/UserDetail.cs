268fc17f212cbc2b4ed77aee429e4bf7ae38cfd3
Rodrigo <rodrigo.fernandes@targetsoftware.com.br>
2015-05-05 13:22:09 -0300

    Projeto iniciado
    Tipo de Projeto: Web
    Iniciado 05/05/2015

---
+++ Common.Chat/Common/UserDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Common.Chat.Model
+{
+    public class UserDetail
+    {
+        public string ConnectionId { get; set; }
+        public string UserName { get; set; }
+    }
+}
